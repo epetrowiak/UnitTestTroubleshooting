74be27161c441da135a314291fd728de377bb713
Eric Petrowiak <e.petrowiak@craneware.com>
2017-09-07 10:36:30 +0100

    simple data source unit test

---
+++ UnitTestAdapterFailedToConnect/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestAdapterFailedToConnect")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestAdapterFailedToConnect")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61745073-6d28-4543-8e63-ca1ec7a6d636")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestAdapterFailedToConnect/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿namespace UnitTestAdapterFailedToConnect
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod, DataSource( //TODO: Reimplement when able to run a data driven test
+             "Microsoft.VisualStudio.TestTools.DataSource.TestCase",
+             "http://TFSServerName:8080/tfs/CollectionName;TeamProject", //TODO: Update this to your tfs
+             "TestCaseId", //TODO: Update to your test case id
+             DataAccessMethod.Sequential)]
+        public void TestMethod1()
+        {
+            var userRole = TestContext.DataRow["userRole"].ToString();
+            Assert.IsNotNull(userRole);
+        }
+    }
+}
